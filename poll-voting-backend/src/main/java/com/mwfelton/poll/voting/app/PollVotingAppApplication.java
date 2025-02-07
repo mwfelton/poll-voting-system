a0647449397eaa9410d828f8043a7c6e320dd7d6
Miles Felton Gerber <mwfelton03@gmail.com>
2025-02-07 22:18:31 +0000

    Initialize Spring Boot backend

---
+++ poll-voting-backend/src/main/java/com/mwfelton/poll/voting/app/PollVotingAppApplication.java
@@ -0,0 +1,13 @@
+package com.mwfelton.poll.voting.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollVotingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollVotingAppApplication.class, args);
+	}
+
+}
---
+++ poll-voting-backend/src/test/java/com/mwfelton/poll/voting/app/PollVotingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwfelton.poll.voting.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollVotingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
