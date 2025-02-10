e824c811e3732925c714f7177a5b8a3eb1824d7a
Miles Felton Gerber <mwfelton03@gmail.com>
2025-02-10 15:52:42 +0000

    CORS policy required

--- poll-voting-backend/src/main/java/com/mwfelton/poll/voting/app/PollVotingAppApplication.java
+++ poll-voting-backend/src/main/java/com/mwfelton/poll/voting/app/PollVotingAppApplication.java
@@ -2,11 +2,33 @@ package com.mwfelton.poll.voting.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PollVotingAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PollVotingAppApplication.class, args);
+	}
+
+	@Configuration
+	public class CorsConfig {
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedOrigins("http://localhost:3000") // Allow frontend
+							.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+							.allowedHeaders("*")
+							.allowCredentials(true);
+				}
+			};
+		}
 	}
 }
